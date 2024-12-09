6ba4f3d0ebb099382443e6ddb337a585fa33f270
XA4APRU4IK <cokolov123123@mail.ru>
2024-12-09 11:38:53 +0700

    Add files via upload

---
+++ Iterator Pattern/Iterator Pattern/Library.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator_Pattern
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library()
+        {
+            books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            if (book != null)
+            {
+                books.Add(book);
+            }
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterator Pattern/Iterator Pattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Iterator_Pattern;
+public class Program
+{
+    public static void Main()
+    {
+        Library library = new Library();
+
+        library.AddBook(new Book("1984", "George Orwell", 1949));
+        library.AddBook(new Book("To Kill a Mockingbird", "Harper Lee", 1960));
+
+        foreach (var book in library)
+        {
+            Console.WriteLine($"{book.Title}, {book.Author}, {book.Year}");
+        }
+    }
+}
